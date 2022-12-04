e8ebe70a9283c010e64a21c6245743ae253bbe83
Josef Zoller <josef@walterzollerpiano.com>
2022-12-04 17:56:53 +0100

    Add solution for day 4

---
+++ day4.hs
@@ -0,0 +1,35 @@
+import Data.List (intersect)
+import Data.List.Split (splitOn)
+import Data.Tuple (swap)
+import Text.Read (readMaybe)
+
+
+isSubset :: (Eq a) => ([a], [a]) -> Bool
+isSubset = (==) <$> fst <*> uncurry intersect
+
+countWith :: (a -> Bool) -> [a] -> Int
+countWith = (length .) . filter
+
+tuple2 :: [a] -> Maybe (a, a)
+tuple2 [x, y] = Just (x, y)
+tuple2 _ = Nothing
+
+
+parse :: String -> Maybe [([Int], [Int])]
+parse = mapM ((tuple2 =<<) . mapM ((uncurry enumFromTo <$>) . (tuple2 =<<) . mapM readMaybe . splitOn "-") . splitOn ",") . lines
+
+part1 :: [([Int], [Int])] -> Int
+-- part1 = countWith $ or . (isSubset <$>) . ([id, swap] <*>) . pure
+part1 = countWith $ (||) <$> isSubset <*> isSubset . swap
+
+part2 :: [([Int], [Int])] -> Int
+part2 = countWith $ not . null . uncurry intersect
+
+main :: IO ()
+main = do
+    input <- readFile "day4.txt"
+    case parse input of
+        Nothing -> putStrLn "Input is corrupted"
+        Just parsed -> do
+            putStrLn $ (show . part1) parsed
+            putStrLn $ (show . part2) parsed
