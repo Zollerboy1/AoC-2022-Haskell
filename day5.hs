92784ae63e5098f4bb34a5ad89ae5b88b369e759
Josef Zoller <josef@walterzollerpiano.com>
2022-12-05 21:21:33 +0100

    Add solution for day 5

---
+++ day5.hs
@@ -0,0 +1,49 @@
+import Helpers (superlines, splitAt2, tuple3, (<.>), ListZipper, getZipper, getList, steps, getElt, replaceElt, mapElt)
+
+import Control.Monad ((<=<))
+import Data.List (partition, transpose)
+import Data.Maybe (fromJust)
+import Data.Tuple (swap)
+import Text.Read (readMaybe)
+
+type Stack = [Char]
+type Command = (Int, Int, Int)
+
+parseStacks :: [String] -> [Stack]
+parseStacks = map (dropWhile (==' ') . init . snd) . filter ((==1) . (`mod` 4) . fst) . zip [0..] . transpose
+
+parseCommands :: [String] -> Maybe [Command]
+parseCommands = mapM (tuple3 <=< mapM (readMaybe . snd) . snd . partition (even . fst) . zip [0..] . words)
+
+parse :: ([String], [String]) -> Maybe ([Stack], [Command])
+parse (s, c) = case (parseStacks s, parseCommands c) of
+    (_, Nothing) -> Nothing
+    (stacks, Just commands) -> Just (stacks, commands)
+
+
+performCommand :: (Int -> Stack -> Maybe ([Char], Stack)) -> Command -> [Stack] -> [Stack]
+performCommand f (n, src, dst) s = fromJust $ do
+        srcZipper <- steps (src - 1) (getZipper s)
+        (chunks, sSrc') <- f n =<< getElt srcZipper
+        dstZipper <- steps (dst - src) =<< replaceElt sSrc' srcZipper
+        getList <$> mapElt (chunks ++) dstZipper
+
+
+part1 :: Int -> Stack -> Maybe ([Char], Stack)
+part1 = flip $ (swap <.>) . flip steps . getZipper
+
+
+part2 :: Int -> Stack -> Maybe ([Char], Stack)
+part2 = (Just .) . splitAt
+
+result :: (Int -> Stack -> Maybe ([Char], Stack)) -> [Stack] -> [Command] -> String
+result = ((map head .) .) . foldl . flip . performCommand
+
+main :: IO ()
+main = do
+    input <- readFile "day5.txt"
+    case parse $ superlines input of
+        Nothing -> putStrLn "Input is corrupted"
+        Just parsed -> do
+            putStrLn (show $ uncurry (result part1) parsed)
+            putStrLn (show $ uncurry (result part2) parsed)
--- helpers.hs
+++ helpers.hs
@@ -1,6 +1,36 @@
-module Helpers (commonElt, countWith, isSubset, split, splitInto, sumMaybe, tuple2, (<.>)) where
+module Helpers (
+    commonElt,
+    split,
+    splitInto,
+    splitAt2,
+    sumMaybe,
+    superlines,
+
+    countWith,
+    isSubset,
+
+    tuple2,
+    tuple3,
+    fst3,
+    snd3,
+    thd3,
+
+    (<.>),
+    (.-.),
+
+    ListZipper,
+    getZipper,
+    getList,
+    goForward,
+    goBack,
+    steps,
+    getElt,
+    replaceElt,
+    mapElt
+) where
 
 import Data.List (intersect)
+import Data.List.Split (splitOn)
 
 
 commonElt :: (Eq a) => [[a]] -> a
@@ -19,8 +49,16 @@ splitInto n xs = h : splitInto (n - 1) t
           c = l `div` n + if l `mod` n == 0 then 0 else 1
           (h, t) = splitAt c xs
 
+splitAt2 :: Int -> Int -> [a] -> ([a], [a], [a])
+splitAt2 n m xs = let n' = min n m;  m' = max n m in (take n' xs, take (m' - n') $ drop n' xs, drop m' xs)
+
+
 sumMaybe :: (Num a) => [Maybe a] -> Maybe a
-sumMaybe = fmap sum . sequence
+sumMaybe = sum <.> sequence
+
+
+superlines :: String -> ([String], [String])
+superlines = (id, tail) .-. break null . lines
 
 
 
@@ -34,7 +72,61 @@ tuple2 :: [a] -> Maybe (a, a)
 tuple2 [x, y] = Just (x, y)
 tuple2 _ = Nothing
 
+tuple3 :: [a] -> Maybe (a, a, a)
+tuple3 [x, y, z] = Just (x, y, z)
+tuple3 _ = Nothing
+
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, y, _) = y
+
+thd3 :: (a, b, c) -> c
+thd3 (_, _, z) = z
+
 
 (<.>) :: Functor f => (b -> c) -> (a -> f b) -> a -> f c
 g <.> f = fmap g . f
 infixr 9 <.>
+
+(.-.) :: (b -> d, c -> e) -> (a -> (b, c)) -> (a -> (d, e))
+(g1, g2) .-. f = \x -> let (y, z) = f x in (g1 y, g2 z)
+infixr 9 .-.
+
+
+type ListZipper a = ([a],[a])
+
+getZipper :: [a] -> ListZipper a
+getZipper = (,[])
+
+getList :: ListZipper a -> [a]
+getList (xs, []) = xs
+getList (xs, b:bs) = getList (b:xs, bs)
+
+goForward :: ListZipper a -> Maybe (ListZipper a)
+goForward (x:xs, bs) = Just (xs, x:bs)
+goForward _ = Nothing
+
+goBack :: ListZipper a -> Maybe (ListZipper a)
+goBack (xs, b:bs) = Just (b:xs, bs)
+goBack _ = Nothing
+
+steps :: Int -> ListZipper a -> Maybe (ListZipper a)
+steps n zipper
+    | n > 0 = goForward zipper >>= steps (n - 1)
+    | n < 0 = goBack zipper >>= steps (n + 1)
+    | otherwise = Just zipper
+
+getElt :: ListZipper a -> Maybe a
+getElt (x:_, _) = Just x
+getElt _ = Nothing
+
+replaceElt :: a -> ListZipper a -> Maybe (ListZipper a)
+replaceElt x (_:xs, bs) = Just (x:xs, bs)
+replaceElt _ _ = Nothing
+
+mapElt :: (a -> a) -> ListZipper a -> Maybe (ListZipper a)
+mapElt f (x:xs, bs) = Just (f x:xs, bs)
+mapElt _ _ = Nothing
